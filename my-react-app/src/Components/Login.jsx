7067196940c362c879f802136c33e551d2d9985b
Srihari Kosuru <kosurusrihari5@gmail.com>
2026-02-20 21:01:15 +0530

    Initial commit - React login form

--- my-react-app/src/Components/Login.jsx
+++ my-react-app/src/Components/Login.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 function Login(){
     const [email,setEmail]=useState('');
     const[password,setPassword]=useState('');
+    console.log(email);
+    console.log(password);
     const formhandle=async()=>{
       try{
         const sendingdate= await fetch("http://localhost:5000/login",
@@ -19,7 +21,7 @@ function Login(){
         );
       
       const data=await sendingdate.json();
-      console.log(data);
+    //   console.log(data);
       
     }catch(e){
         console.error("Error",e);
